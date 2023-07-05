aeaf8c86558b9506f2a9eaaad1adf411b023ce14
LautyFarías247 <lauty0618@gmail.com>
2023-07-05 20:45:43 -0300

    update client image

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,15 @@
 import express,{ Application } from 'express'
 import authRouter from './routes/auth'
-import clientRouter from './routes/client'
+import clientsRouter from './routes/client'
 import lawyersRouter from './routes/lawyer'
-import subscriptionRouter from './routes/subscription'
+import subscriptionsRouter from './routes/subscription'
 import meetsRouter from './routes/meet'
-import morgan from 'morgan'
-import { verifyToken } from './utils/authUtils'
+import paymentsRouter from './routes/payment'
 
+import morgan from 'morgan'
+import fileUpload from 'express-fileupload'
+import path from 'path'
+import { verifyToken } from './utils/authUtils'
 const app: Application = express()
 
 //settings
@@ -15,11 +18,19 @@ app.set('port',3001)
 //middlewares
 app.use(morgan('dev'))
 app.use(express.json())
+
+const uploadDir = path.join(__dirname, '../upload');
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: uploadDir
+}));
+
 // routes
 app.use('/api/auth',authRouter)
-app.use('/api/clients',clientRouter)
+app.use('/api/clients',clientsRouter)
 app.use('/api/lawyers',lawyersRouter)
-app.use('/api/subscriptions',subscriptionRouter)
+app.use('/api/subscriptions',subscriptionsRouter)
 app.use('/api/meets',meetsRouter)
-
+app.use('/api/payments', paymentsRouter)
 export default app
--- api/src/controllers/client.controller.ts
+++ api/src/controllers/client.controller.ts
@@ -1,4 +1,6 @@
 import { Request,Response } from "express";
+import { uploadImage } from "../libs/cloudinary";
+import fs from 'fs-extra'
 import Client from '../models/Client'
 
 export const getClients = async (req: Request,res: Response) => {
@@ -34,11 +36,43 @@ export const getClientById = async (req: Request,res: Response) => {
 export const updateClientData = async (req: Request,res: Response) => {
 	const { _id } = req.params
 	try {
-		const updatedClient = await Client.findByIdAndUpdate(_id,req.body)
-		res.status(200).json(updatedClient);
+
+		// const updatedClient = await Client.findByIdAndUpdate(_id,req.body)
+		// res.status(200).json(updatedClient);
 	} catch (error: any) {
 		console.log(error);
 		res.status(400).json(error.message);
+	}
+}
+
+export const updateClientImage = async (req: Request, res: Response) => {
+	const {_id} = req.params
+	try {	
+		const file = req.files?.image
+		let image 
+		//upload image
+		if(req.files?.image){
+			const uploadedFile = Array.isArray(file)
+			? file[0]
+			: file;
+			
+			const tempFilePath = uploadedFile?.tempFilePath as string;
+			const {public_id, secure_url} = await uploadImage(tempFilePath);
+			image = {
+				public_id,
+				url: secure_url
+			}
+		//delete file
+		fs.remove(tempFilePath)
+		//update client
+		const client = await Client.findByIdAndUpdate(_id, {image}, {new: true})
+
+			res.status(200).json(client);
+		} else {
+			throw new Error('Nonexistent image')
+		}
+	} catch (error) {
+		
 	}
 }
 
