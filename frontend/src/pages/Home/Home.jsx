03f6f60da05c51491fcbe40d3e094b7378b33a63
Gonzalo <garciacalvog@hotmail.com>
2023-07-12 17:48:44 -0300

    cambio de imgs

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,15 +6,22 @@ import Box from '@mui/material/Box'
 import './styles.home.css'
 import { Button, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import portadaHome from '../../assets/portadaHome.jpg'
 
 const Home = () => {
   return (
     <>
-      <Box as='section' className="boxPrincipalImg" /* sx={{ backgroundImage: 'Url(../../home1.jpg)',backgroundSize: "cover", backgroundRepeat: 'no-repeat', width: '100%', height: '45em'}} */>
-      <Navbar isLogged={true}/>
-        <Typography as="h1">Abogados</Typography>
-        <Typography>Servicios de compra y venta de servicios legales</Typography>
-        <Button variant="contained" ><Link to={'/login'}>login</Link></Button>
+      <Box as='section' className="boxPrincipalImg"  > 
+        <Navbar isLogged={true}/>
+        <Box sx={{zIndex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '30em' }}>
+          <Typography as="h1" sx={{fontSize: '2em'}}>Abogados</Typography>
+          <Typography as="h2" sx={{fontSize: '1.2em'}}>Servicios de compra y venta de servicios legales</Typography>
+          <Button className='btnLinkToLogin' variant="contained" sx={{ margin: '3em 0 0em 0' , background: '#FAFF00', width: '248px', borderRadius: 0, '&:hover' : {background: '#FAFF00' }}} >
+            <Link style={{ color: 'black', width: '100%', textDecoration: 'none'}} to={'/login'}>
+              login
+            </Link>
+          </Button>
+        </Box>
       </Box>
       <AboutServices />
       <Contact />
