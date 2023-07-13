5612541d37cab6e7a34226f4a703fdc9abbca58b
Franco <francofleitasguti@gmail.com>
2023-07-13 16:35:12 -0300

    cambios vistas y Navbar funcional

--- frontend/src/redux/userReducer.js
+++ frontend/src/redux/userReducer.js
@@ -1,30 +1,30 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
   endpoints: (builder) => ({
     addUser: builder.mutation({
       query: (input) => ({
-        url: "api/auth/signup",
-        method: "POST",
-        body: input,
-      }),
+        url: 'api/auth/signup',
+        method: 'POST',
+        body: input
+      })
     }),
     getUser: builder.mutation({
       query: (credentials) => ({
-        url: "api/auth/signin",
-        method: "POST",
-        body: credentials,
-      }),
+        url: 'api/auth/signin',
+        method: 'POST',
+        body: credentials
+      })
     }),
     getUserById: builder.query({
-      query: ( _id ) => `api/clients/${_id}`,
-    }),
-  }),
-});
+      query: (_id) => `api/clients/${_id}`
+    })
+  })
+})
 
-export const selectUser = (state) => state.userApi.data;
+export const selectUser = (state) => state.userApi
 
 export const { useAddUserMutation, useGetUserMutation, useGetUserByIdQuery } =
-  userApi;
+  userApi
