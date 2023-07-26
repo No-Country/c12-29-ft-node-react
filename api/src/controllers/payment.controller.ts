ca0ec1a55f51a7bcadba1d708daa484a9c9efc9c
LautyFarias247 <lautarofarias743@gmail.com>
2023-07-26 20:52:08 -0300

    mp suscriptions, edit: lawyer model

--- api/src/controllers/payment.controller.ts
+++ api/src/controllers/payment.controller.ts
@@ -1,13 +1,45 @@
-import mercadopago from 'mercadopago'
+
 import dotenv from 'dotenv'
+import axios from 'axios';
+import { Request,Response } from 'express';
 dotenv.config()
 
-const {MP_TOKEN_SECRET} = process.env;
+const { ACCESS_TOKEN_MP } = process.env;
 
-export const createPayment = async () => {
-	console.log(MP_TOKEN_SECRET);
+export const createPayment = async (req: Request,res: Response) => {
+	try {
+		
+		const {email, subscriptionName, subscriptionPrice} = req.body
+		const url = "https://api.mercadopago.com/preapproval";
 	
-	mercadopago.configure({
-		access_token: MP_TOKEN_SECRET || ""
-	})
+		const headers = {
+			"Authorization": `Bearer ${ACCESS_TOKEN_MP}`,
+			"Content-Type": "application/json"
+		};
+		
+		const data = {
+			reason: subscriptionName,
+			payer_email: email,
+			auto_recurring: {
+				frequency: 1,
+				frequency_type: "months",
+				transaction_amount: subscriptionPrice,
+				currency_id: "ARS"
+			},
+			back_url: "https://www.yoursite.com",
+			status: "pending"
+		};
+		
+		const subscription = await axios.post(url, data, { headers })
+		console.log(subscription.data);
+		
+		res.status(200).json(subscription.data)
+	} catch (error:any) {
+		return res.status(400).json(error.response.data)
+
+	};
 }
+
+export const webhook = () => {
+	
+} 
