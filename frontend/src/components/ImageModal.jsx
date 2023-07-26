8eb7a6501a86549bc2c0d1af1a68a0454994f031
Franco <francofleitasguti@gmail.com>
2023-07-26 18:45:25 -0300

    Form specialty & description to lawyer

--- frontend/src/components/ImageModal.jsx
+++ frontend/src/components/ImageModal.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import ImgUploader from './ImgUploader';
-import { useUpdateLawyerImageMutation } from '../redux/userReducer';
-import Zoom from '@mui/material/Zoom';
+import { useEffect, useState } from 'react'
+import Backdrop from '@mui/material/Backdrop'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import Fade from '@mui/material/Fade'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import ImgUploader from './ImgUploader'
+import { useUpdateLawyerImageMutation } from '../redux/userReducer'
+import Zoom from '@mui/material/Zoom'
 
 const style = {
   position: 'absolute',
@@ -16,33 +16,32 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  p: 4,
-};
+  p: 4
+}
 
-export default function ImageModal() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const [ updateLawyerImage, { data, error, isSuccess, isLoading}] = useUpdateLawyerImageMutation({
-    fixedCacheKey: 'image-uploading',
+export default function ImageModal () {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const [updateLawyerImage, { data, error, isSuccess, isLoading }] = useUpdateLawyerImageMutation({
+    fixedCacheKey: 'image-uploading'
   })
-  useEffect( () => {
-    isSuccess||error? handleClose() : null
-  },[isSuccess, error])
+  useEffect(() => {
+    isSuccess || error ? handleClose() : null
+  }, [isSuccess, error])
 
   return (
-    <Box sx={{ display:'flex', flexDirection:'column', alignItems:'center', my:'3em', borderRadius:'4px'}}>
-      <Typography variant="h1" sx={{fontSize:'1.5em', color:'white', marginBottom:'1em'}}>Subir foto, debe ser cuadrada</Typography>
-      <Button 
-        onClick={handleOpen} 
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: '3em', borderRadius: '4px' }}>
+      <Button
+        onClick={handleOpen}
         variant='contained'
         sx={{
-          background:'#FAFF00', 
-        color:'black', 
-        '&:hover':{background: '#cbda16', boxShadow:'5',   }
+          background: '#FAFF00',
+          color: 'black',
+          '&:hover': { background: '#cbda16', boxShadow: '5' }
         }}
       >
-        Abrir
+        Seleccionar Foto
       </Button>
       <Modal
         aria-labelledby="transition-modal-title"
@@ -53,10 +52,10 @@ export default function ImageModal() {
         slots={{ backdrop: Backdrop }}
         slotProps={{
           backdrop: {
-            timeout: 500,
-          },
+            timeout: 500
+          }
         }}
-        sx={{'& .css-1wnsr1i':{ borderRadius:'4px'} }}
+        sx={{ '& .css-1wnsr1i': { borderRadius: '4px' } }}
       >
         <Fade in={open}>
           <Box sx={style}>
@@ -76,5 +75,5 @@ export default function ImageModal() {
         </Fade>
       </Modal>
     </Box>
-  );
+  )
 }
