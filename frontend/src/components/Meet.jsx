5df48dccb43ee954c05ee5ed27d59d794318b46b
Franco <francofleitasguti@gmail.com>
2023-07-27 19:58:50 -0300

    rendering meets en LawyerPanel

---
+++ frontend/src/components/Meet.jsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from '@mui/material'
+import { dateFormater } from '../utils/dateFormater'
+
+const Meet = ({ data }) => {
+  const clientData = data.clientId
+  const { day, month, year, hour, minutes } = dateFormater(data.date)
+
+  return (
+    <Box sx={{ minWidth: '320px', border: '1px solid white', p: 1, borderRadius: '4px', m: 1 }}>
+      <Typography>Nombre: {clientData.firstname} {clientData.lastname}</Typography>
+      <Typography>email: {clientData.email}</Typography>
+      <Typography>Fecha: {day} de {month}, {year} a las {hour}:{minutes}</Typography>
+    </Box>
+  )
+}
+
+export default Meet
