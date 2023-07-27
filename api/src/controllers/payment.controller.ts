26b2c89bea7c223a1b0303bd039c89c51514abef
LautyFarias247 <lautarofarias743@gmail.com>
2023-07-27 15:05:24 -0300

    mp

--- api/src/controllers/payment.controller.ts
+++ api/src/controllers/payment.controller.ts
@@ -1,45 +1,70 @@
 
 import dotenv from 'dotenv'
-import axios from 'axios';
+import mercadopago from 'mercadopago'
 import { Request,Response } from 'express';
+import Lawyer from '../models/Lawyer';
+import Subscription from '../models/Subscription';
 dotenv.config()
 
 const { ACCESS_TOKEN_MP } = process.env;
 
 export const createPayment = async (req: Request,res: Response) => {
 	try {
-		
-		const {email, subscriptionName, subscriptionPrice} = req.body
-		const url = "https://api.mercadopago.com/preapproval";
-	
-		const headers = {
-			"Authorization": `Bearer ${ACCESS_TOKEN_MP}`,
-			"Content-Type": "application/json"
-		};
-		
-		const data = {
-			reason: subscriptionName,
-			payer_email: email,
-			auto_recurring: {
-				frequency: 1,
-				frequency_type: "months",
-				transaction_amount: subscriptionPrice,
-				currency_id: "ARS"
+		const { subscription,userId } = req.body
+		mercadopago.configure({
+			access_token: "TEST-7499841737015948-072020-c04ebf0bc641a47760e53cae3c39b2a5-1415911684"
+		})
+
+		const result = await mercadopago.preferences.create({
+			items: [
+				{
+					title: subscription.name,
+					unit_price: subscription.price,
+					currency_id: "ARS",
+					quantity: 1
+				}
+			],
+			external_reference: userId,
+			back_urls: {
+				success: "https://c12-29-ft-node-react.vercel.app/",
+				pending: "https://c12-29-ft-node-react.vercel.app/",
+				failure: "https://c12-29-ft-node-react.vercel.app/",
 			},
-			back_url: "https://www.yoursite.com",
-			status: "pending"
-		};
-		
-		const subscription = await axios.post(url, data, { headers })
-		console.log(subscription.data);
-		
-		res.status(200).json(subscription.data)
-	} catch (error:any) {
-		return res.status(400).json(error.response.data)
+			notification_url:
+				"https://8cf0-2800-810-557-2bcd-d408-33a1-710f-ca4c.ngrok.io/api/payments/webhook",
+		})
+
+		res.status(200).json(result)
+	} catch (error: any) {
+		console.log(error);
+
+		return res.status(400).json(error)
 
 	};
 }
 
-export const webhook = () => {
+export const webhook = async (req: Request,res: Response) => {
+	try {
+		const payment = req?.query;
+		const id = Number(payment["data.id"])
+		console.log("------------------WEBHOOK------------------");
+		if (payment.type === "payment") {
+			const { body } = await mercadopago.payment.findById(id);
+			console.log(body);
+
+			const { status,external_reference,additional_info } = body;
+
+			if (status === "approved") {
+
+				const subscription = await Subscription.findOne({name: additional_info.items[0].title})
+
+				const newLawer = await Lawyer.findByIdAndUpdate(external_reference,{ subscription },{ new: true })
 	
-} 
+				return res.status(200).json(newLawer);
+			}
+		}
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json(error);
+	}
+};
